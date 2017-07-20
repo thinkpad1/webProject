5728004578270d6e17d20c21d3e7d123ce78e192
ramazan <ramazanc@hotmail.com>
2017-07-20 23:53:15 +0200

    test

--- src/main/java/de/cinardere/web/MainApp.java
+++ src/main/java/de/cinardere/web/MainApp.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class MainApp {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MainApp.class, args);
 	}
